89493e592b285aed3427fe44cf3c13297107f231
franckalainai <franckalain.ai@gmail.com>
2021-05-14 15:40:20 +0000

    responding to URL parameters

--- app.js
+++ app.js
@@ -7,12 +7,36 @@ const fs = require('fs');
 const port = 3000;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
         data: {
             tours
+        }
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    
+    //convert id to a number
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    //if(id >= tours.length)
+    if(!tour){
+        res.status(404).json({
+            status: "Failed",
+            message: "Invalid ID"
+        });
+    }
+
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
         }
     });
 });
