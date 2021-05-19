d9f52cecde58a1e5fe8890c5e9d4f16dfec8c416
franckalainai <franckalain.ai@gmail.com>
2021-05-19 18:02:00 +0000

    creating document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-    if(!req.body.name || !req.body.price){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        });
-    }
-    next();
-}
-
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -40,14 +30,22 @@ exports.getTour = (req, res) => {
     */
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+    try{
+        const newTour = await Tour.create(req.body);
     res.status(201).json({
         status: 'success',
-        /*data: {
+        data: {
             tour: newTour
         }
-        */
     });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid data sent'
+        })
+    }
+    
 };
 
 exports.updateTour = (req, res) => {
