d840201533c925ae6a38b38c8ab8c5a533abb5b5
franckalainai <franckalain.ai@gmail.com>
2021-05-19 13:39:34 +0000

    connect database with express app

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const moogoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', 
+    process.env.DATABASE_PASSWORD);
+moogoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB connection successful'));
 
 const app = require('./app');
 
