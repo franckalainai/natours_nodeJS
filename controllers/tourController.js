23c82911993251cf62d4c5bcb21d747a0d7ae7cd
franckalainai <franckalain.ai@gmail.com>
2021-05-19 16:07:36 +0000

    refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,16 +1,4 @@
-const fs = require('fs');
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-exports.checkID = (req, res, next, val) => {
-    console.log(`Tour id is: ${val}`);
-    if(req.params.id * 1 > tours.length){
-        res.status(404).json({
-            status: "Failed",
-            message: "Invalid ID"
-        });
-    }
-    next();
-}
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
     if(!req.body.name || !req.body.price){
@@ -27,10 +15,11 @@ exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         requestedAt: req.requestTime,
-        results: tours.length,
+        /*results: tours.length,
         data: {
             tours
         }
+        */
     });
 };
 
@@ -39,6 +28,7 @@ exports.getTour = (req, res) => {
     
     //convert id to a number
     const id = req.params.id * 1;
+    /*
     const tour = tours.find(el => el.id === id);
     
     res.status(200).json({
@@ -47,22 +37,17 @@ exports.getTour = (req, res) => {
             tour
         }
     });
+    */
 };
 
 exports.createTour = (req, res) => {
-    // body is a property available in req request
-    //console.log(req.body);
-    const newID = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newID}, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    }); 
+    res.status(201).json({
+        status: 'success',
+        /*data: {
+            tour: newTour
+        }
+        */
+    });
 };
 
 exports.updateTour = (req, res) => {
