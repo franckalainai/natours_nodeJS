a3bb599ea92e0f2590c2b4f6587db78f0072597a
franckalainai <franckalain.ai@gmail.com>
2021-05-19 14:28:48 +0000

    create a simple tour Schema

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const moogoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
+
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>', 
     process.env.DATABASE_PASSWORD);
-moogoose.connect(DB, {
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 }).then(() => console.log('DB connection successful'));
 
 const app = require('./app');
+
+// creation schema
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price:{
+        type: Number,
+        require: [true, 'A tour must have a price']
+    }
+});
+
+const tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
