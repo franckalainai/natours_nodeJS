15b8efe88fb5fbd9cd021d018953d322a0e5658d
franckalainai <franckalain.ai@gmail.com>
2021-05-20 18:04:16 +0000

    advanced filtering API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,12 +2,22 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
+        console.log(req.query);
         // BUILD QUERY
+        // 1) Filtering
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
         
-        const query = Tour.find(queryObj);
+        // Advanced filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        console.log(JSON.parse(queryStr));
+
+        const query = Tour.find(JSON.parse(queryStr));
+        
+        //{ difficulty: 'easy', duration: { gte: '5' } } 
         // EXECUTE QUERY
         const tours = await query;
         
