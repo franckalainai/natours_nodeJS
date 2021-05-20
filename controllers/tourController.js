2135638943eaecd93ac1d7aa2f13fccf0ac2e77c
franckalainai <franckalain.ai@gmail.com>
2021-05-20 16:40:26 +0000

    API filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,17 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
-    res.status(200).json({
+        // BUILD QUERY
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+        
+        const query = Tour.find(queryObj);
+        // EXECUTE QUERY
+        const tours = await query;
+        
+        // SEND RESPONSE
+        res.status(200).json({
         status: 'success',
         //requestedAt: req.requestTime,
         results: tours.length,
@@ -41,7 +50,7 @@ exports.getTour = async (req, res) => {
 exports.createTour = async (req, res) => {
     try{
         const newTour = await Tour.create(req.body);
-    res.status(201).json({
+        res.status(201).json({
         status: 'success',
         data: {
             tour: newTour
